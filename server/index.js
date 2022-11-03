d88b71b37acc30a6eea1a5fc7bdaee91fec692ae
Levy-Naibei <levynaibei2@gmail.com>
2022-11-03 11:47:14 +0300

    chore(main): server setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// import express from 'express';
+// import dotenv from 'dotenv';
+
+// const app = express();
+// dotenv.config({path: './.env'})
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome all!")
+// });
+
+// const port = process.env.PORT || 5000;
+// app.listen(
+//     port,
+//     console.log(`Dev Server running on port ${port}`)
+// );
