8483ae6ac1befdf1781c8b8eeab7c7f1f4783f15
Levy-Naibei <levynaibei2@gmail.com>
2022-11-16 23:05:58 +0300

    chore(typeDefs): added schema type definitions

--- server/graphQl/schema/index.js
+++ server/graphQl/schema/index.js
@@ -2,13 +2,70 @@ import { gql } from 'apollo-server';
 
 const typeDefs = gql`
     type User {
-        _id: Int!,
+        _id: Int!
         name: String!
         email: String!
     }
 
-    type Query { getUsers: [User]! },
-    type Mutation { createUser(name: String!, email: String!): User! }
+    type AuthResponse {
+        token: String!
+        name: String!
+        image: String
+    }
+
+    type node {
+        state: String!
+        title: String!
+        number: Int!
+        permalink: String!
+        author: Author!
+    }
+
+    type Author {
+        url: String!
+        avatarUrl: String!
+        resourcePath: String!
+    }
+
+    input RepoInfo {
+        orgName: String!
+        repoName: String!
+        labelTag: String!
+        numberOfPRs: Int!
+    }
+
+    input PRinfo {
+        orgName: String!
+        repoName: String!
+        prNumber: String!
+        displayNumber: Int!
+    }
+
+    type prAssignees {
+        name: String!
+        avatarUrl: String!
+     }
+
+    type labelInfo { 
+        name: String!
+        description: String!
+    }
+
+    type pullRequest {
+        state: String!
+        labels: [labelInfo]
+        assignees: [prAssignees]
+    }
+
+    type Query { 
+        viewPrAssignees(input: PRinfo!): pullRequest
+        viewLabeledTasks(input: RepoInfo!): [node]
+    }
+
+    type Mutation {
+        signup(name: String!, email: String!): User!
+        login(personalAccessToken: String!): AuthResponse!
+    }
 
     type Schema { query: Query, mutation: Mutation }
 `
